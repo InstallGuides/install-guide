03b093f81b7db78142eb047469372701cdfc9116
Daniel Kehoe <daniel@danielkehoe.com>
2021-01-05 10:05:15 +0000

    wip: try https://jspm.dev/lit-element (no versionb pinning)

--- assets/scripts/yax-hack.js
+++ assets/scripts/yax-hack.js
@@ -1,4 +1,4 @@
-import { LitElement, html } from 'https://cdn.skypack.dev/lit-element';
+import { LitElement, html } from 'https://jspm.dev/lit-element';
 
 export class YaxHack extends LitElement {
 	render() {
