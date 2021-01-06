fa3bfea140e652f6114f36046b8a989f72948330
Daniel Kehoe <daniel@danielkehoe.com>
2021-01-06 07:51:54 +0000

    wip: add unsafe-html from skypack

--- assets/scripts/yax-hack.js
+++ assets/scripts/yax-hack.js
@@ -1,6 +1,7 @@
 import prismjs from 'https://cdn.skypack.dev/prismjs';
 import commonmark from 'https://cdn.skypack.dev/commonmark@0.29.1';
 import { LitElement, html } from 'https://cdn.skypack.dev/lit-element@2';
+import { unsafeHTML } from 'https://cdn.skypack.dev/lit-html@1/directives/unsafe-html.js';
 
 export class YaxHack extends LitElement {
   render() {
