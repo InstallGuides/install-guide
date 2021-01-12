083651f2b7f1fd1c0b73d0e390e1fa37b8edcdba
Daniel Kehoe <daniel@danielkehoe.com>
2021-01-12 18:00:35 +0800

    wip: try jspm cli 1

--- assets/scripts/yax-hack.js
+++ assets/scripts/yax-hack.js
@@ -1,13 +1,11 @@
-import { LitElement, html } from 'https://cdn.skypack.dev/lit-element@2.4.0';
-
+import { LitElement, html } from 'https://jspm.dev/lit-element';
 export class YaxHack extends LitElement {
   render() {
     return html`
     <h2>Test</h2>
-    <code>import { LitElement, html } from 'https://cdn.skypack.dev/lit-element@2.4.0';</code>
+    <code>import { LitElement, html } from 'https://jspm.dev/lit-element';</code>
     `;
   }
 }
-
 customElements.define('yax-hack', YaxHack);
 
