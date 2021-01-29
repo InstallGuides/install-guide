ba48691c1131308b10ca38dcd5a76f6405051cc7
Daniel Kehoe <daniel@danielkehoe.com>
2021-01-29 07:18:20 +0000

    test again, loading prismjs and commonmark from SkyPack, other libraries from jspm.dev

--- assets/scripts/yax-markdown.js
+++ assets/scripts/yax-markdown.js
@@ -1,4 +1,4 @@
-import * as prismjs from 'https://jspm.dev/prismjs';
+import * as prismjs from 'https://cdn.skypack.dev/prismjs';
 import * as commonmark from 'https://cdn.skypack.dev/commonmark';
 import { LitElement, html } from 'https://jspm.dev/lit-element';
 import { unsafeHTML } from 'https://jspm.dev/lit-html/directives/unsafe-html.js';
